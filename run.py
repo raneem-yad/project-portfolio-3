7ef3c3b463ab509fd5af69d485ee0e24506bfd6d
raneem-yad <eng.raneem.yad@gmail.com>
2024-02-22 15:00:51 +0100

    Show start APP messages

--- run.py
+++ run.py
@@ -1,3 +1,35 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from utils import Colors
+from utils import Message
+from tabulate import tabulate
+
+# make table by string
+# ome_data = [['08:01', 1.00, 32], ['08:02', 1.01, 33], ['08:03', 1.02, 33]]
+# h = ['Time', 'x', 'n']
+
+# print('{:<10s} {:<5s} {:<5s}'.format(*h))
+# for list_ in some_data:
+#     print('{:<10s} {:.2f} {:<5d}'.format(*list_))
+
+# make lines around the table
+
+
+
+main_page_messages = [
+    ["Login\n", 1],
+    ["Create Account\n", 2],
+    ["App preview\n", 3]
+]
+# Table headers
+headers = [ "How we can Help you", "Press"]
+
+# Print the table
+
+
+
+def main():
+    print(Colors.UNDERLINE+Colors.BOLD+Colors.RED + "\n Welcome to TaskTracker App!\n" + Colors.RESET)
+    # print(tabulate(main_page_messages, headers=headers, tablefmt="grid",showindex="always"))
+    print(tabulate(main_page_messages, headers=headers, tablefmt="grid"))
+
+
+main()
--- utils.py
+++ utils.py
@@ -1,12 +1,22 @@
 class Colors:
-  black = "\u001b[30m"
-  red = "\u001b[31m"
-  green = "\u001b[32m"
-  yellow = "\u001b[33m"
-  blue = "\u001b[34m"
-  magenta = "\u001b[35m"
-  cyan = "\u001b[36m"
-  white = "\u001b[37m"
-  
-  reset =  "\u001b[0m"
-  def rgb(r, g, b): return f"\u001b[38;2;{r};{g};{b}m"
+    BLACK = "\u001b[30m"
+    RED = "\u001b[31m"
+    GREEN = "\u001b[32m"
+    YELLOW = "\u001b[33m"
+    BLUE = "\u001b[34m"
+    MAGENTA = "\u001b[35m"
+    CYAN = "\u001b[36m"
+    WHITE = "\u001b[37m"
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    RESET = "\u001b[0m"
+    def rgb(r, g, b): return f"\u001b[38;2;{r};{g};{b}m"
+
+
+# class Message:
+#     def __init__(self, msg, command):
+#         self.msg = msg
+#         self.command = command
+
+#     def get_message(self):
+#         return [self.msg, self.command]
