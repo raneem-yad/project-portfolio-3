c8489334ada1df58a99009a68f5b5ce9f25700c7
raneem-yad <eng.raneem.yad@gmail.com>
2024-02-24 17:21:03 +0100

    add new user to excel sheets

--- user/user_func.py
+++ user/user_func.py
@@ -1,41 +1,130 @@
+import re
 import utils.connecting as conn
-# which sheet in our spreadsheet love_sandwiches
 
+SHEET_NAME = 'users'
 connected_sheet = conn.get_sheet()
-sales = connected_sheet.worksheet('users')
+sales_sheet = connected_sheet.worksheet(SHEET_NAME)
 
-def get_all_user():
-    data = sales.get_all_values()
-    return data
+ID_COLUMN_INDEX = 1
+USERNAME_COLUMN_INDEX = 2
+EMAIL_COLUMN_INDEX = 4
+PASSWORD_COLUMN_INDEX = 5
+
+
+def get_all_users():
+    """
+    Retrieves all user data from the spreadsheet.
+
+    Returns:
+        list: A list containing all user data.
+    """
+    return sales_sheet.get_all_values()
+
+
+def get_all_emails():
+    """
+    Retrieves all email addresses from the spreadsheet.
+
+    Returns:
+        list: A list containing all email addresses.
+    """
+    return sales_sheet.col_values(EMAIL_COLUMN_INDEX)
+
+
+def get_all_usernames():
+    """
+    Retrieves all usernames from the spreadsheet.
+
+    Returns:
+        list: A list containing all usernames.
+    """
+    return sales_sheet.col_values(USERNAME_COLUMN_INDEX)
+
+
+def get_last_id():
+    """
+    Retrieves the last ID from the spreadsheet.
+
+    Returns:
+        str: The last ID value.
+    """
+    return sales_sheet.col_values(ID_COLUMN_INDEX)[-1]
 
 
 def get_login_info():
-    """Get the username and password column to check if the user exist or not
     """
-    # username is in column 2
-    # password is in column 5
-    columns = []
-    for ind in [2,5]:
-        column = sales.col_values(ind)
-        columns.append(column)
-    return columns
+    Retrieves login information (usernames and passwords) from the spreadsheet.
+
+    Returns:
+        Tuple: A typle containing two lists, the first list contains usernames and the second list contains passwords.
+    """
+
+    usernames_column = sales_sheet.col_values(USERNAME_COLUMN_INDEX)
+    passwords_column = sales_sheet.col_values(PASSWORD_COLUMN_INDEX)
+    return usernames_column, passwords_column
+
 
 def login(username, password):
-    users = get_login_info()
-    usernames = users[0]
-    passwords = users[1]
-    
+    """
+    Validates the provided username and password.
+
+    Args:
+        username (str): The username to validate.
+        password (str): The password to validate.
+
+    Returns:
+        bool: True if the username and password are valid, False otherwise.
+    """
+    usernames, passwords = get_login_info()
+
     if username in usernames:
-        # Get the index of the item to compare it with password
         index = usernames.index(username)
-        # print(f"we found a user with username '{username}' with index: {index}")
-        if (passwords[index] == password):
-            # print(f"the password is {password}")
+        if passwords[index] == password:
             return True
-        else: 
-            # print(" the password is wrong") 
+        else:
             return False
     else:
-        # print(f"'{username}' is not in the users.")
         return False
-    
+
+
+def is_valid_email(email):
+    """
+    Checks if the provided email address is valid and not already in use.
+
+    Args:
+        email (str): The email address to validate.
+
+    Returns:
+        bool: True if the email is valid and not already in use, False otherwise.
+    """
+    regex_email = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    return re.fullmatch(regex_email, email) and email not in get_all_emails()
+
+
+def username_exists(username):
+    """
+    Checks if the provided username already exists in the database.
+
+    Args:
+        username (str): The username to check.
+
+    Returns:
+        bool: True if the username exists, False otherwise.
+    """
+    return username in get_all_usernames()
+
+
+def add_new_user(fullname, username, email, password):
+    """
+    Adds a new user to the Sheet.
+
+    Args:
+        fullname (str): The full name of the user.
+        username (str): The username of the user.
+        email (str): The email address of the user.
+        password (str): The password of the user.
+    """
+    last_id = int(get_last_id())
+    new_user_id = last_id + 1
+    new_user = [new_user_id, username, fullname, email, password]
+    conn.update_worksheet(new_user, SHEET_NAME)
