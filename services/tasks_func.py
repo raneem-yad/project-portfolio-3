66e991511054a2632c19de59dd2412b967947583
raneem-yad <eng.raneem.yad@gmail.com>
2024-02-26 14:00:17 +0100

    add new task and update status functionality

--- services/tasks_func.py
+++ services/tasks_func.py
@@ -1,6 +1,8 @@
+from datetime import datetime
 import utils.connecting as conn
 import services.users_func as user
 from utils.theme import Colors
+
 
 DEMO_DATA = [
     [1, 'Prepare report', 'Compile monthly sales report', '2024-02-20', 'No'],
@@ -17,7 +19,7 @@ SHEET_NAME = 'tasks'
 connected_sheet = conn.get_sheet()
 tasks_sheet = connected_sheet.worksheet(SHEET_NAME)
 
-
+ID_COLUMN_INDEX = 1
 USER_ID_COLUMN_INDEX = 2
 TASK_COLUMN_INDEX = 3
 PRIORITY_COLUMN_INDEX = 5
@@ -60,10 +62,21 @@ def show_status_label(status):
     else :
         return f"{Colors.RED} No {Colors.RESET}"
     
+def get_last_id():
+    """
+    Retrieves the last ID from the spreadsheet.
+
+    Returns:
+        str: The last ID value.
+    """
+    return tasks_sheet.col_values(ID_COLUMN_INDEX)[-1]
+
+def get_user_id_for_task(username): 
+    return user.get_user_id_per_username(username)
 
 def get_tasks_per_date(username , date):
     # get user id 
-    user_id = user.get_user_id_per_username(username)
+    user_id = get_user_id_for_task(username)
     # retieve data from excel sheet 
     ids = get_all_users_id()
     tasks = get_all_tasks()
@@ -77,3 +90,25 @@ def get_tasks_per_date(username , date):
             tasks_data.append([tasks[i],show_priority_label(priorities[i]),show_status_label(status[i])])
     # return this tasks
     return tasks_data
+
+
+
+def add_new_task(task, details,priority,username): 
+    last_id = int(get_last_id())
+    new_task_id = last_id + 1
+    user_id = get_user_id_for_task(username)
+    today_date = str(datetime.now().date())
+    status = '0'
+
+    new_task = [new_task_id, user_id, task, details, priority,today_date,status]
+    conn.update_worksheet(new_task, SHEET_NAME)
+    
+    
+def update_status(task_name): 
+    # finding the task in table (get the row)
+    cell = tasks_sheet.find(task_name)
+    # choice the status column 
+    updated_task_row_poition = cell.row
+    updated_task_col_poition = STATUS_COLUMN_INDEX
+    # update the value 
+    tasks_sheet.update_cell(updated_task_row_poition, updated_task_col_poition, '1')
